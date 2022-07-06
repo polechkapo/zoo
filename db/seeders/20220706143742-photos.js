8fac3cadce5ad46c8bf0f47c2a2b74b7151659c4
Alex <petenkov2822@gmail.com>
2022-07-06 18:17:30 +0300

    prepare from seeders done

--- db/seeders/20220706143742-photos.js
+++ db/seeders/20220706143742-photos.js
@@ -4,47 +4,47 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('Photos', [{
       animal_id: 1,
-      img_href: '',
+      img_href: '../../public/images/Тигр-фото1.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       animal_id: 1,
-      img_href: '',
+      img_href: '../../public/images/Тигр-фото2.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       animal_id: 1,
-      img_href: '',
+      img_href: '../../public/images/Тигр-фото3.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       animal_id: 2,
-      img_href: '',
+      img_href: '../../public/images/горилла1.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       animal_id: 2,
-      img_href: '',
+      img_href: '../../public/images/горилла2.png',
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       animal_id: 2,
-      img_href: '',
+      img_href: '../../public/images/горилла3.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       animal_id: 3,
-      img_href: '',
+      img_href: '../../public/images/пин1.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       animal_id: 3,
-      img_href: '',
+      img_href: '../../public/images/пин2.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
       animal_id: 3,
-      img_href: '',
+      img_href: '../../public/images/пин3.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});

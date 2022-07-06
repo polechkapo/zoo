73e5ae73353a13bfdc0af591401cbd4fa3904e25
Alex <petenkov2822@gmail.com>
2022-07-06 15:43:24 +0300

    model tariff create

---
+++ db/migrations/20220706124258-create-tariff.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tariffs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      day_type: {
+        type: Sequelize.TEXT
+      },
+      human_type: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tariffs');
+  }
+};
---
+++ db/models/tariff.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tariff extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Tariff.init({
+    day_type: DataTypes.TEXT,
+    human_type: DataTypes.TEXT,
+    price: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Tariff',
+  });
+  return Tariff;
+};

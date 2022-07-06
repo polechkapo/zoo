74fcb9707a2094a10a1f9720ec12c08aca9e1e23
Yury <businesselbgus@mail.ru>
2022-07-06 16:38:38 +0300

    first_step

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
-require('@babel/register');
+require('dotenv').config(); // для чтения .env файла
+require('@babel/register'); // бэбил для jsx файлов
 
 const app = require('express')();
 const configApp = require('./config/config_app'); // подключаем конфиги
 
-const homeRouter = require('./routes/view/home_router');
+// const homeRouter = require('./routes/view/home_router');
+const authRouter = require('./routes/view/auth_router')
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -14,8 +15,9 @@ app.get('/', (req, res) => {
   res.redirect('/home');
 });
 
-app.use('/home', homeRouter);
 
+// app.use('/home', homeRouter);
+app.use('/auth', authRouter)
 app.listen(PORT, () => {
   console.log(`Сервер шуршит на ${PORT}`);
 });

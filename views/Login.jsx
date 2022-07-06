be51a325ad173f4f4b560bafd08a5f689714ed25
Alex <petenkov2822@gmail.com>
2022-07-06 20:45:22 +0300

    login navbar fix

--- views/Login.jsx
+++ views/Login.jsx
@@ -6,11 +6,11 @@ module.exports = function Login({ admin }) {
 
     <form method="post" action="/auth/login">
       <div className="">
-        <label htmlFor="log" className="">Логин</label>
+        <label htmlFor="log" className="">Login</label>
         <input type="text" id="log" className="" aria-describedby="loginHelp" name="login" />
       </div>
       <div className="">
-        <label htmlFor="password" className="">Пароль</label>
+        <label htmlFor="password" className="">Password</label>
         <input type="password" id="password" className="" name="password" />
       </div>
       <button type="submit" className="">Submit-this-shit</button>

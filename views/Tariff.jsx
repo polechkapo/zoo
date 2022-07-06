1d5298f81ab6a6384cb9460eeb0580ccd2be653a
Yury <businesselbgus@mail.ru>
2022-07-06 19:27:22 +0300

    two_step___

--- views/Tariff.jsx
+++ views/Tariff.jsx
@@ -21,7 +21,7 @@ module.exports = function Tariff() {
           <td>@mdo</td>
         </tr>
         <tr>
-          <th scope="row">2</th>
+          <th scope="row">2</th> 
           <td>Jacob</td>
           <td>Thornton</td>
           <td>@fat</td>

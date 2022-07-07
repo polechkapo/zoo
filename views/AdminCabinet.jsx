adaf25d1c224a49bca49c566fe5a3d3d69678f25
mac <mac@MacBook-Pro-mac.local>
2022-07-07 17:46:11 +0300

    divCadHab

--- views/AdminCabinet.jsx
+++ views/AdminCabinet.jsx
@@ -1,21 +1,25 @@
 const React = require('react');
 const Layout = require('./Layout');
+const { Context } = require('../middlewares/ssr');
 
 module.exports = function AdminCabinet({ tariff }) {
+  const { admin } = React.useContext(Context);
   return (
     <Layout>
-      <div className="cab">
-        <p>привет Админ</p>
-        <p>{tariff[0].day_type}</p>
-        <form>
-          {tariff.map((el, i) => (
-            <div className="" key={i}>
-              <label htmlFor={`label-${i}`} className="" />
-              <input type="text" id={`label-${i}`} className="" name={`input${i}`} defaultValue={el.price} />
-            </div>
-          ))}
-        </form>
-      </div>
+      {admin ? (
+        <div className="cab">
+          <p>привет Админ</p>
+          <p>{tariff[0].day_type}</p>
+          <form>
+            {tariff.map((el, i) => (
+              <div className="" key={i}>
+                <label htmlFor={`label-${i}`} className="" />
+                <input type="text" id={`label-${i}`} className="" name={`input${i}`} defaultValue={el.price} />
+              </div>
+            ))}
+          </form>
+        </div>
+      ) : <p>ты не админ</p>}
     </Layout>
   );
 };

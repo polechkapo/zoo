499f5d7daa443e24f38ddd059a2cc2d302efcd9c
Yury <businesselbgus@mail.ru>
2022-07-07 16:24:40 +0300

    foto add

--- routes/view/animallist_router.js
+++ routes/view/animallist_router.js
@@ -2,12 +2,13 @@ const animalsRouter = require('express').Router();
 const { Animal } = require('../../db/models');
 const { Photo } = require('../../db/models');
 const AnimalList = require('../../views/AnimalList');
+const AnimalCard = require('../../views/Animal')
 
 animalsRouter.route('/')
   .get(async (req, res) => {
     const animals = await Animal.findAll();
     const photosAll = await Photo.findAll();
-    const photosList = photosAll.filter((item => item.img_href.includes('1')));
+    const photosList = photosAll.filter(((item) => item.img_href.includes('1')));
     res.renderComponent(AnimalList, { animals, photosList });
   });
 
@@ -15,7 +16,8 @@ animalsRouter.route('/:id/info')
   .get(async (req, res) => {
     const animalId = req.params.id;
     const animal = await Animal.findByPk(animalId);
-    res.renderComponent(Animal, { animal });
+    
+    res.renderComponent(AnimalCard, { animal });
   });
 
 module.exports = animalsRouter;
--- views/Layout.jsx
+++ views/Layout.jsx
@@ -8,6 +8,9 @@ module.exports = function Layout({ title, children }) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" />
         <title>{ title }</title>
       </head>
       <header>
